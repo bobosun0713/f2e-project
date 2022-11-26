02701918e8a3df11d8587bde6f5bd80351c14fa1
bobosun0713 <chiabo.sun@gmail.com>
2022-11-26 22:07:36 +0800

    feat: add base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ['./src/**/*.{vue,ts,tsx,js,jsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#007fab',
+          dark: '#003a4f',
+          tint: '#b0d2de'
+        },
+        secondary: {
+          DEFAULT: '#ffc37d',
+          dark: '#a46039',
+          tint: '#ffe2a9'
+        },
+        highlight: {
+          DEFAULT: '#ff5136',
+          dark: '#cd331a',
+          tint: '#ffb5a4'
+        },
+        gray: {
+          100: '#000000',
+          80: '#666666',
+          60: '#666666',
+          40: '#999999',
+          20: '#cccccc',
+          0: '#ffffff'
+        }
+      }
+    }
   },
   plugins: []
 };
