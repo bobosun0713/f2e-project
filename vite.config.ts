f473c5eafef3ad19ca94adc6789f6fe6ab5002b3
bobosun0713 <chiabo.sun@gmail.com>
2023-04-30 22:54:47 +0800

    feat: modify base url on deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,19 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 8080
-  },
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  console.log(mode);
+
+  return {
+    base: mode === 'production' ? '/f2e-project/' : '/',
+    server: {
+      port: 8080
+    },
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
+  };
 });
